6d9c6a33df03ff88d9d5ddc9269fb6f6eeedea3b
Grigoriy Kaygorodtsev <grigoriy.kaygorotsev@gmail.com>
2024-02-14 15:38:44 +0300

    Commiting

--- src/test/java/ru/netology/rest/mobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/mobileBankApiTestV4.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
-class MobileBankApiTestV4 {
+class mobileBankApiTestV4 {
     @Test
     void shouldReturnDemoAccounts() {
         // Given - When - Then
@@ -16,8 +16,10 @@ class MobileBankApiTestV4 {
                 .when()
                 .get("/demo/accounts")
                 // Проверки
+                // код теста
                 .then()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
+
 }
